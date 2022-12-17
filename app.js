a787a50ed56afe39576dba26a45814ffeaf5ff62
Kevin Chagas <kevinchagas@yahoo.comKevinKevin>
2022-12-17 17:08:28 -0600

    webb

---
+++ app.js
@@ -0,0 +1,29 @@
+//What is HTTP?
+'Hyper text transfer protocol is how you send or retrive data from a server'
+
+//What is a URL?
+'Uniform Resource Locator, an address for an internet resource'
+
+//What is DNS?
+'Domain Name System, system that converts URLs into IP addresses'
+
+//What is Query String?
+'allows you to pass key-value into the URL'
+
+//What are two HTTP verbs and how are they different?
+'Get - retrive data from the server'
+'Post- send some data to the server'
+
+//What is an HTTP request?
+'a request from a client to a server that follows HTTP protocol'
+
+//What is an HTTP response?
+'a response from a client to a server that follows HTTP protocol'
+
+//What is an HTTP header? Give a couple examples of request and response headers you have seen.
+'Headers provide additional info on requests and responses'
+'Request headers: Host, Cookie, Cache-Control'
+'Response headers: Set-cookie, Cache-control'
+
+//What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+'The browser resolves the name into an IP address using DNS. The browser also makes a request to the same IP address. The server then sends a response.'
